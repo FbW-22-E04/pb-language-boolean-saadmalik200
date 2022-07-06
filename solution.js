28e27dbbc9ff6b8355da7ee0271ea73701482941
Saad Malik <saad_malik200@hotmail.com>
2022-07-06 13:41:41 +0200

    exercise 1

---
+++ solution.js
@@ -0,0 +1,18 @@
+console.log(3 == "3");
+console.log(3 === "3");
+// all of the above
+// none of the above
+// a
+
+let myVar = true;
+
+console.log(!myVar ? "good morning" : "good evening");
+
+let firstName = "" || "John" || 0;
+let zero = "" || false || 0;
+let emptyStr = "" && false && "Hello World";
+let seven = 75 && "nine" && 7;
+
+// console.log(firstName, emptyStr, zero);
+
+console.log(firstName, zero, emptyStr, seven);
